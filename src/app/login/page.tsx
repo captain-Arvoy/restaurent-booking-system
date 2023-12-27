c73352ef6ee48951ae490b1b41ffd06d4cba148d
captain-Arvoy <accofstardust@gmail.com>
2023-12-27 13:38:09 +0530

    added padlock icon

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,10 @@
 import {useState} from 'react'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
+import Image from 'next/image'
+const onLogin = ()=>{
+    console.log("logged in")
+}
 export default function Login (){
     const[user, setUser] = useState({
         username: null,
@@ -10,8 +14,9 @@ export default function Login (){
     return(
         <>
         <div className='flex flex-col items-center justify-center py-2 min-h-screen'>
-            <TextField id="username" label="username" variant="outlined" value={user.username} onChange={(e) => setUser({...user, username: e.target.value})}/>
-            <TextField id="password" label="password" variant="outlined" type='password' value={user.password} className='m-4' onChange={(e)=> setUser({...user, password: e.target.value})}/>
+            <Image src="/lock.png" width={50} height={50} alt='padlock-icon' />
+            <TextField id="username" label="username" variant="outlined" value={user.username} className='m-4' onChange={(e) => setUser({...user, username: e.target.value})}/>
+            <TextField id="password" label="password" variant="outlined" type='password' value={user.password} className='mb-4' onChange={(e)=> setUser({...user, password: e.target.value})}/>
             <Button disabled={!user.username || !user.password} id="submit-button" variant='outlined' onClick={onLogin}>login</Button>
         </div>
         </>
